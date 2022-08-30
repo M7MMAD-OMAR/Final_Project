e8806c6bb228a3d7a044a1060af81e72512894ed
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-08-30 17:02:01 +0300

    edit files

---
+++ database/factories/ProductDetailsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductDetailsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->titleMale,
+            'description' => $this->faker->text,
+            'product_id' => $this->faker->unique()->randomElement(range(1, 100))
+        ];
+    }
+}
