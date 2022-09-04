c93e949888b7a521d05f52bb9309e435089c32e7
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-04 08:02:47 +0300

    reset to old site

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-   return view('index');
+   return view('welcome');
 });
 
 Route::group(['middleware' => 'api'], function () {
