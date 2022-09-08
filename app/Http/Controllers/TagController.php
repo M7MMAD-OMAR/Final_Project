8421b87ecd5c97b3e56750e0bbb207dd7911d360
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-09 01:19:24 +0300

    Finish admin panel

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class TagController extends Controller
 {
     public function store(Request $request) {
-
+        $tag =  Tag::create([
+//            'tmdb_id' => $request['id'],
+            'tag_name' => $request['tag_name'],
+            'slug' => Str::slug($request['tag_name']),
+        ]);
+        return back()->with('success', 'Ok it\'s saved');
     }
 }
