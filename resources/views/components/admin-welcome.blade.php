8bfbf5db3adfd546feef9cc69ee4b75eedb56c83
muham <muhammadaaa727@gmail.com>
2023-01-07 11:58:23 +0300

    change logo from admin page

--- resources/views/components/admin-welcome.blade.php
+++ resources/views/components/admin-welcome.blade.php
@@ -1,6 +1,6 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
     <div class="text-xl font-bold text-green-700">
-        Movie Gallery
+        Movie App
     </div>
 
     <div class="mt-8 text-2xl">
